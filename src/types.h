5cc10ff95585a23d828325226597519af3e73d33
amichaelt <amichaeltm@gmail.com>
2012-07-17 09:43:01 -0500

    Update version number.

--- src/types.h
+++ src/types.h
@@ -26,7 +26,7 @@
 #include <QColor>
 
 const QString progName = "BDSup2Sub++";
-const QString progNameVer = progName + " 1.0.0 RC3";
+const QString progNameVer = progName + " 1.0.0";
 const QString authorDate = "0xdeadbeef, mjuhasz, Adam T.";
 const QString oldIniName = "bdsup2sup.ini";
 const QString iniName = "bdsup2sup++.ini";
