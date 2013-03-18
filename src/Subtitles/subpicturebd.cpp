9ebe1cb08a5cdd22700bdba778b30bc1c5e3aeb6
paradoxic4l <paradoxical@gmail.com>
2013-03-18 15:14:14 -0500

    Add new members to copy constructors.

--- src/Subtitles/subpicturebd.cpp
+++ src/Subtitles/subpicturebd.cpp
@@ -27,7 +27,9 @@ SubPictureBD::SubPictureBD(const SubPictureBD *other) :
     SubPicture(other),
     type(other->type),
     numWindows(other->numWindows),
-    paletteUpdate(other->paletteUpdate)
+    paletteUpdate(other->paletteUpdate),
+    compState(other->compState),
+    paletteID(other->paletteID)
 {
     for (int i = 0; i < other->imageObjectList.size(); ++i)
     {
@@ -47,7 +49,9 @@ SubPictureBD::SubPictureBD(const SubPictureBD &other) :
     SubPicture(other),
     type(other.type),
     numWindows(other.numWindows),
-    paletteUpdate(other.paletteUpdate)
+    paletteUpdate(other.paletteUpdate),
+    compState(other.compState),
+    paletteID(other.paletteID)
 {
     for (int i = 0; i < other.imageObjectList.size(); ++i)
     {
